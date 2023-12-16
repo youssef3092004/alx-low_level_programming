64282fea5db5d4a71f245ddf49aedbab8dc93cb3
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-16 05:33:03 -0800

    g

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - Entry
+ *
+ * describtion: A C program that prints function.
+ *
+ * return: Always 0 (success)
+ */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
