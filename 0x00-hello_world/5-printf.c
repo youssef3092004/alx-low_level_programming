5b388c0f65456abd246c60591acab8d6c2236013
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-16 05:19:54 -0800

    f

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * blaaaaaaaaaaaaaaa
+ *
+ * blaaaaaaaaaaaaaaaaaaa
+ * 
+ * blaaaaaaaaaaaaaaaaaa
+ * */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
