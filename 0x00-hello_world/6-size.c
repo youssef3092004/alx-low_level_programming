d50867c7d671e44701d3dc349a22e9885c9a7b8d
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-16 06:34:54 -0800

    defdf

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * discribtion : A C program that prints with put function
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	printf("Size of a char: ",sizeof(char));
+    	printf("Size of an int: ",sizeof(int));
+   	 printf("Size of a long int: ",sizeof(long int));
+    	printf("Size of a long long int: ",sizeof(long long int));
+    	printf("Size of a float: ",sizeof(float));
+	return (0);
+}
