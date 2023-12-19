5c994052e9768886197d57cd6956d2dce38ea989
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-19 09:45:45 -0800

    g

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Description: print all alphabet letters
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char x = 1;
+
+	while (x <= 9)
+	{
+		putchar(x + '0');
+		x++;
+	}
+	putchar('\n');
+
+	return (0);
+}
