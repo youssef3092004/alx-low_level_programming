9ec3a88981be07ebc4a61b077cbbf99c564557d8
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-20 11:26:41 -0800

    k

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include"min.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_alphabet(void)
+{
+	char x = 97;
+
+	while (x <= 122)
+	{
+		putchar(x);
+		x++;
+	}
+	putchar('\n');
+}
