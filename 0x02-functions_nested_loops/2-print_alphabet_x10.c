c3c994ef35f4a384bddd91b420477522535389cf
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-20 13:02:58 -0800

    f

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase,
+ * i++ increment the variable i by 1.
+ */
+
+for (int i = 1 ; i <= 10; i++)
+{
+char x = 97;
+
+for (; x < 122 ; x++)
+{
+putchar(x);
+}
+putchar('\n');
+}
