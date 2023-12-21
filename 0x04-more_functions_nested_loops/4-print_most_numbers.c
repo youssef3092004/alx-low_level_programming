eb2eee6db7c261d0d705230b502cf518453776b7
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-21 10:26:08 -0800

    d

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 - 9
+ *		only using _putchar twice
+ *
+ * Return: always 0 (success)
+*/
+void print_numbers(void)
+{
+char x = 48;
+
+for (; x <= 57 ; x++)
+{
+if (x == 2 || x == 4)
+{
+i++;
+}
+_putchar(x);
+}
+_putchar('\n');
+}
