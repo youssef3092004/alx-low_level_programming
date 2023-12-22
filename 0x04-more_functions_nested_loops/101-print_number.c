55324d24ad9867654803eddf1daa2b9c5b45b6ba
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-22 13:03:35 -0800

    g

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+*print_number - prints an integer
+*
+*@n: input integer
+*
+* Return: void
+**/
+
+void print_number(int n)
+{
+unsigned int num = n;
+
+/*1st check negativity*/
+if (n < 0)
+{
+_putchar('-');
+num = -num;
+}
+
+/*print 1st few digits*/
+if ((num / 10) > 0)
+print_number(num / 10);
+
+/*prints last digit*/
+_putchar((num % 10) + 48);
+}
---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+*print_diagonal - prints a diagonal in the terminal
+*
+*@n: input integer
+*
+* Return: void
+**/
+
+void print_diagonal(int n)
+{
+int diagchr, space;
+
+if (n <= 0)
+{
+_putchar('\n');
+}
+else
+{
+for (diagchr = 1; diagchr <= n; diagchr++)
+{
+for (space = 1; space < diagchr; space++)
+_putchar(' ');
+_putchar(92);
+_putchar('\n');
+}
+}
+}
