45849d145fc655dc066e8756ebddc9dc19b9ec8b
youssef <youssefahmedabdelkader25@gmail.com>
2023-12-22 06:33:35 -0800

    g

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -13,17 +13,14 @@ for (i = 1 ; i <= 100 ; i++)
 if (i % 3 == 0 && i % 5 == 0)
 {
 printf("FizzBuzz");
-continue;
 }
 else if (i % 3 == 0)
 {
 printf("Fizz");
-continue;
 }
 else if (i % 5 == 0)
 {
 printf("Buzz");
-continue;
 }
 else
 printf("%d", i);
