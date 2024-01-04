07fff221dd104f149886845fd54dd96ba6533b61
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-04 07:55:53 -0800

    f

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * leet - leet
+ * @s: string
+ *
+ * Return: char value
+ */
+char *leet(char *s)
+{
+int i;
+
+for (i = 0; i < s[i]; i++)
+{
+if (s[i] == 'a' || s[i] == 'A')
+s[i] = 4;
+else if (s[i] == 'e' || s[i] == 'E')
+s[i] = 3;
+else if (s[i] == 'o' || s[i] == 'O')
+s[i] = 0;
+else if (s[i] == 't' || s[i] == 'T')
+s[i] = 7;
+else if (s[i] == 'l' || s[i] == 'L')
+s[i] = 1;
+}
+return (s);
+}
