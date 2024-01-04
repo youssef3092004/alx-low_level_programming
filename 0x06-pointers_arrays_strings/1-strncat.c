ae677f83de86f581f04374beb75eb36943a01083
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-04 05:26:10 -0800

    f

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -11,12 +11,13 @@
 
 char *_strncat(char *dest, char *src, int n)
 {
+int i, j;
 
-size_t dest_len = strlen(dest);
-size_t i;
-
-for (i = 0 ; i < n && src[i] != '\0' ; i++)
-dest[dest_len + i] = src[i];
-dest[dest_len + i] = '\0';
+i = 0;
+while(dest[i])
+i++;
+for (j = 0 ; j < n && src[j] != '\0' ; j++)
+dest[i + j] = src[j];
+dest[i + j] = '\0';
 return (dest);
 }
