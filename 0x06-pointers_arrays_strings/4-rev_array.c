707839383f4a981332cfed726d0bd3ce0835af9a
youssef ahmed <116092909+youssef3092004@users.noreply.github.com>
2024-01-04 16:03:09 +0200

    Update 4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,9 @@
 
 void reverse_array(int *a, int n)
 {
-int i ,j, temp;
-
-for (i = 0, j = n-1 ; i < j ; i++, j--)
+int i;
+for (i = n - 1 ; i <= 0 ; i--)
 {
-temp = a[i];
-a[i] = a[j];
-a[j] = temp;
+putchar(a[i]);  
 }
 }
