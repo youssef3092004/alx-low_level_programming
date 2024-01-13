83f9067da480dddb11a50f66ae25bee68a3ea01a
youssef ahmed <116092909+youssef3092004@users.noreply.github.com>
2024-01-13 23:39:15 +0200

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,13 @@
  */
 char *_strchr(char *s, char c)
 {
-int i = 0;
-while (s[i != '\0'])
+while (*s)
 {
-if (s[i++] == c)
-{
-return (s + i);
+if (*s == c)
+return (s);
+s++;
 }
-return ('\0');
-}
+if (c == '\0')
+return (s);
+return (NULL);
 }
