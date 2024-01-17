b1f711d11638684e66902a5fb8a10de7bc856dff
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-17 09:08:41 -0800

    g

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include<stdlib.h>
+#include<stdio.h>
+/**
+ * main - Entry point
+ * @argc: this is a int
+ * @argv: this is a array
+ * Return: always 0 (Succes)
+ */
+int main(int argc, char *argv[])
+{
+int sum;
+if (sum == 3)
+{
+sum = (atio(argv[1]) * atio(argv[2]));
+printf("%d\n", sum);
+}
+else
+{
+printf("Error\n");
+return (1);
+}
+return (0);
+}
