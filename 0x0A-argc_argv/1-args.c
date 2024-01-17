08807f5c03773c0051ab1837fe12383f49c75c10
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-17 08:01:57 -0800

    h

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: this is a int
+ * @argv: this is a array
+ * Return: always 0 (Succes)
+ */
+int main(int argc, char const *argv[])
+{
+printf("%d\n", argc)
+return (0);
+}
