2cb5e66e3249f345e823d06f33fd1f65b27db194
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-17 08:04:52 -0800

    fg

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,12 +2,12 @@
 
 /**
  * main - Entry point
- * @argc: this is a int
- * @argv: this is a array
+ * @argc: that is counter
+ * @argv: pointer
  * Return: always 0 (Succes)
  */
 int main(int argc, char const *argv[])
 {
-printf("%d\n", argc)
+printf("%d\n", argc - 1)
 return (0);
 }
