27a903c7354c57d55f6e462e04db577e282b444e
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-17 07:30:53 -0800

    find

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+/**
+ * main - prints program name followd by new line
+ * @argc: this is a int
+ * @argv: this is a array
+ * return : 0
+ */
+int main(int argc, char const * argv[])
+{
+(void)argc;
+printf("%s\n", argv[0]);
+return (0);
+}
