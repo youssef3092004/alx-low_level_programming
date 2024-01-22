9b8ba1b059a161779b393b38ac43be7d388e4d98
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-22 10:29:51 -0800

    d

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,18 @@
+#include "DOG_H"
+/**
+ * init_dog -  initialize a variable of dog
+ *@d: init of dog
+ *@name: name of dog
+ *@age: age of dog
+ *@owner: owner of dog
+ *return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d)
+{
+d->name = name;
+d->age = age;
+d->owner = owner;
+}
+}
