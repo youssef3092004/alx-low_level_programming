c2fe3f3cf00834602b2928f94445b890d2ce1a69
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-22 09:42:18 -0800

    g

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - it's a stract and her name dog
+ * @name: dog name
+ * @age: dog age
+ * @owner: the owner of dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+#endif
