421737d9c2b35ade9913b333f5bfad5d723f03cf
youssef <youssefahmedabdelkader25@gmail.com>
2024-01-25 19:50:38 +0200

    g

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef VRIADIC_FUNCTION_H
+#define VARIADIC_FUNCTION_H
 
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
