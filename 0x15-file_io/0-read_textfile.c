13540267dacb6af87dc8b2ff6a21750fe6d22793
youssef ahmed <116092909+youssef3092004@users.noreply.github.com>
2024-02-12 18:57:38 +0200

    Add files via upload

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**
  * read_textfile - reads a text file and prints the letters
  * @filename: filename.
@@ -8,21 +9,28 @@
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-int x;
-ssize_t bye;
-char buf[READ_BUF_SIZE * 8];
+	int fd;
+	ssize_t nrd, nwr;
+	char *buf;
 
-if (!filename || !letters)
-{
-return (0);
-}
-x = open(filename, O_RDONLY);
-if (x == -1)
-{
-return (0);
-}
-bye = read(fd, buf[0], letters);
-bye = write(STDOUT_FILENO, &buf[0], bye);
-close(x);
-return (bye);
+	if (!filename)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	buf = malloc(sizeof(char) * (letters));
+	if (!buf)
+		return (0);
+
+	nrd = read(fd, buf, letters);
+	nwr = write(STDOUT_FILENO, buf, nrd);
+
+	close(fd);
+
+	free(buf);
+
+	return (nwr);
 }
