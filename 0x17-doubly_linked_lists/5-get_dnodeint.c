39b24b99f56c34b6821c1219150f59edff846027
youssef <youssefahmedabdelkader25@gmail.com>
2024-03-24 21:59:00 +0200

    final

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,20 +6,23 @@
  * @head: Pointer to the head of the doubly linked list.
  * @index: The index of the node to retrieve.
  *
- * Return: If the node at the specified index exists, return,
+ * Return: If the node at the specified index exists, return
  * a pointer to that node.
  *         Otherwise, return NULL.
  */
 
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index) {
-  dlistint_t *cur = head;
-  size_t i = 0;
-  while (cur->next) {
-    if (i == index)
-      return (cur);
-    else {
-      cur = cur->next;
-      i++;
-    }
-  }
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+dlistint_t *cur = head;
+size_t i = 0;
+while (cur->next)
+{
+if (i == index)
+return (cur);
+else
+{
+cur = cur->next;
+i++;
+}
+}
 }
