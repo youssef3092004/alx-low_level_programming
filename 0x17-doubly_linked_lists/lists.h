c7120149e878b8111a1a595698e7f515caaa0ec2
youssef <youssefahmedabdelkader25@gmail.com>
2024-03-24 21:45:13 +0200

    final

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -3,11 +3,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct dlistint
+typedef struct dlistint_s
 {
 	int n;
-	struct dlistint *prev;
-	struct dlistint *next;
+	struct dlistint_s *prev;
+	struct dlistint_s *next;
 } dlistint_t;
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
