f1174bb4112a001676496370942491691f89eadc
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-23 19:11:06 +0300

    fianl

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,7 +6,7 @@ int binary_search(int *array, size_t size, int value)
     {
     if (array == NULL)
         return (-1);
-    int first = 0, last = 0, middle = 0;
+    size_t  first = 0, last = 0, middle = 0;
     while (last <= middle) {
         first = 0, last = size - 1,
         middle = (first + last) / 2;
@@ -18,17 +18,4 @@ int binary_search(int *array, size_t size, int value)
             first = middle + 1;
     }
     return (-1);
-}
-
-int main(void)
-{
-    int array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    printf("Found %d at index: %d\n\n", 2, binary_search(array, size, 2));
-    printf("Found %d at index: %d\n\n", 5, binary_search(array, 5, 5));
-    printf("Found %d at index: %d\n", 999, binary_search(array, size, 999));
-    return (EXIT_SUCCESS);
 }
