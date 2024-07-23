a4829053f2cc2d81a86248f4481a2b8dbb738f48
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-23 17:08:19 +0300

    final

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,20 +13,20 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-    size_t i;
+size_t i;
 
-    for (i = 0; i < size; i++)
-    {
-        if (array[i] == value)
-        {
-            printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
-            return (i);
-        }
-        else
-        {
-            printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
-        }
-    }
+for (i = 0; i < size; i++)
+{
+if (array[i] == value)
+{
+printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+return (i);
+}
+else
+{
+printf("Value checked array[%lu] = [%d]\n", (unsigned long)i, array[i]);
+}
+}
 
-    return (-1);
+return (-1);
 }
