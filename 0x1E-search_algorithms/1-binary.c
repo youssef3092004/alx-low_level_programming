cf3b05f33ddcfb39024977b665bf342522157046
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-23 19:44:29 +0300

    final

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,56 +2,56 @@
 #include <stdio.h>
 
 /**
- * print_array - Prints the current part of the array being searched.
- * @array: The array to be printed.
- * @first: The starting index of the current subarray.
- * @last: The ending index of the current subarray.
- */
+* print_array - Prints the current part of the array being searched.
+* @array: The array to be printed.
+* @first: The starting index of the current subarray.
+* @last: The ending index of the current subarray.
+*/
 void print_array(int *array, size_t first, size_t last)
 {
-    size_t i;
+size_t i;
 
-    printf("Searching in array: ");
-    for (i = first; i <= last; i++)
-    {
-        printf("%d", array[i]);
-        if (i < last)
-            printf(", ");
-    }
-    printf("\n");
+printf("Searching in array: ");
+for (i = first; i <= last; i++)
+{
+printf("%d", array[i]);
+if (i < last)
+printf(", ");
+}
+printf("\n");
 }
 
 /**
- * binary_search - Performs a binary search on a sorted integer array.
- * @array: The sorted integer array to be searched.
- * @size: The number of elements in the array.
- * @value: The value to search for in the array.
- *
- * Return: The index of the first occurrence of the value in the array,
- * or -1 if not found.
- */
+* binary_search - Performs a binary search on a sorted integer array.
+* @array: The sorted integer array to be searched.
+* @size: The number of elements in the array.
+* @value: The value to search for in the array.
+*
+* Return: The index of the first occurrence of the value in the array,
+* or -1 if not found.
+*/
 int binary_search(int *array, size_t size, int value)
 {
-    size_t first, last, middle;
+size_t first, last, middle;
 
-    if (array == NULL)
-        return (-1);
+if (array == NULL)
+return (-1);
 
-    first = 0;
-    last = size - 1;
+first = 0;
+last = size - 1;
 
-    while (first <= last)
-    {
-        print_array(array, first, last);
-        middle = (first + last) / 2;
+while (first <= last)
+{
+print_array(array, first, last);
+middle = (first + last) / 2;
 
-        if (array[middle] == value)
-            return (middle);
-        else if (array[middle] > value)
-            last = middle - 1;
-        else
-            first = middle + 1;
-    }
+if (array[middle] == value)
+return (middle);
+else if (array[middle] > value)
+last = middle - 1;
+else
+first = middle + 1;
+}
 
-    return (-1);
+return (-1);
 }
