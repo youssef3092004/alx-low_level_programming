cec6c50ff976fb30e6199021c3a705ca9ade4842
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-23 19:13:33 +0300

    fianl

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -2,20 +2,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+* binary_search - Performs a binary search on a sorted integer array.
+* @array: The sorted integer array to be searched.
+* @size: The number of elements in the array.
+* @value: The value to search for in the array.
+*
+* Return: The index of the first occurrence of the value in the array,
+* or -1 if not found.
+*/
 int binary_search(int *array, size_t size, int value)
-    {
-    if (array == NULL)
-        return (-1);
-    size_t  first = 0, last = 0, middle = 0;
-    while (last <= middle) {
-        first = 0, last = size - 1,
-        middle = (first + last) / 2;
-        if (array[middle] == value)
-            return (middle);
-        if (array[middle] > value)
-            last = middle - 1;
-        else
-            first = middle + 1;
-    }
-    return (-1);
+{
+size_t first, last, middle;
+
+if (array == NULL)
+return (-1);
+
+first = 0;
+last = size - 1;
+
+while (first <= last)
+{        
+middle = (first + last) / 2;
+if (array[middle] == value)
+return (middle);
+else if (array[middle] > value)
+last = middle - 1;
+else
+first = middle + 1;
+}
+
+return (-1);
 }
