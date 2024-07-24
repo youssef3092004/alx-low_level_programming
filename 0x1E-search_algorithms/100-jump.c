a16e4171cf8b5d89a9b0fb5e48a5fba59a192932
youssef <youssefahmedabdelkader25@gmail.com>
2024-07-24 21:55:19 +0300

    final

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -36,15 +36,3 @@ int jump_search(int *array, size_t size, int value)
     return (-1);
 }
 
-int main(void)
-{
-    int array[] = {
-        0, 1, 2, 3, 4, 5, 6, 7, 8, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
-
-    printf("Found %d at index: %d\n\n", 6, jump_search(array, size, 6));
-    printf("Found %d at index: %d\n\n", 1, jump_search(array, size, 1));
-    printf("Found %d at index: %d\n", 999, jump_search(array, size, 999));
-    return (0);
-}
